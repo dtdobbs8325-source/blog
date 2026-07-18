b7ead9138d4eace70b7d8628edd479c8aee44a14
Derek Dobbs <dtdobbs8325@gmail.com>
2026-07-18 06:12:08 -0500

    updated port

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const cors = require('cors');
 const fs = require('fs');
 const path = require('path');
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 // Middleware to handle JSON data and serve your front-end files
 app.use(express.json());
